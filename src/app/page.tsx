8e6ab9f46b090575309a5eedc2ab18c9a1edba56
KaranChouhan018 <jeemain145@gmail.com>
2025-05-17 01:18:36 +0530

    refactor: remove unused font and clean up page layout
    This commit removes the unused `Geist_Mono` font import from the layout file and cleans up the `page.tsx` by commenting out the `FeelingCarousel` component. These changes streamline the code and improve maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import FeelingCarousel from "@/components/FeelingCoursel";
+
 import Video from "@/components/Video";
 export default function Home() {
   return (
     <main className="w-screen h-screen overflow-hidden">
       <Video/>
-      {/* <FeelingCarousel/> */}
+    
     </main>
   );
 }
